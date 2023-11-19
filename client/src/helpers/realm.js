a4fcd5404c889377c32c6b723f9a2c02681e4de3
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2023-11-19 20:25:31 +0000

    anonymous login

--- client/src/helpers/realm.js
+++ client/src/helpers/realm.js
@@ -4,22 +4,28 @@ import React, { useContext, useState } from "react"
 
 const RealmAppContext = React.createContext(null)
 
-const RealmApp = ({ children }) => {
+const RealmApp = async ({ children }) => {
     // const REALM_APP_ID = process.env.REACT_APP_REALM_APP_ID
     const app = new RealmWeb.App({ id:"application-1-xpzpg"})
     const [user, setUser] = useState(null)
 
-    const logIn = async (email, password) => {
-        const credentials = RealmWeb.Credentials.emailPassword(email, password)
-        try {
-            await app.logIn(credentials)
-            setUser(app.currentUser)
-            return app.currentUser
-        } catch (e) {
-            setUser(null)
-            return null
-        }
-    }
+    // const logIn = async (email, password) => {
+    //     const credentials = RealmWeb.Credentials.emailPassword(email, password)
+    //     try {
+    //         await app.logIn(credentials)
+    //         setUser(app.currentUser)
+    //         return app.currentUser
+    //     } catch (e) {
+    //         setUser(null)
+    //         return null
+    //     }
+    // }
+    const credentials = Realm.Credentials.anonymous();
+try {
+  const user = await app.logIn(credentials);
+} catch(err) {
+  console.error("Failed to log in", err);
+}
 
     const logOut = () => {
         if (user !== null) {
