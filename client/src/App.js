28b620c92bf56e252d037fc21d77c673368f2ed8
jagz97 <68725607+jagz97@users.noreply.github.com>
2023-11-26 02:50:45 -0800

    final setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React, { useEffect, useState } from "react"
 
 import { useMongoDB } from "./helpers/mongodb"
@@ -5,6 +6,10 @@ import { useRealmApp } from "./helpers/realm"
 import { CitySearch } from "./components/CountrySearch"
 import { LogInForm } from "./components/LogInForm"
 import { AirportList } from "./components/AirportList"
+import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+
+
+
 
 
 
@@ -13,22 +18,52 @@ function App() {
     const { db } = useMongoDB()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [airports, setAiprots] = useState([])
+    
     const [countrySearch, setCountrySearch] = useState([])
     
     const [selectedCountryCode, setSelectedCountryCode] = useState(null); // New state to store the selected country code
 
+    
+
+    
+    
+
     useEffect(() => {
         async function wrapQuery() {
             if (user && db) {
-                const allAirports = await db.collection("157b").find({"surface": {"$regex": "^g", "$options": "i"}})
-                console.log("Airports by surface turf", allAirports); 
-                setAiprots(allAirports)
-
+                
                 // Use the selected country code in the country_search function
                 const search = await user.functions.country_search(selectedCountryCode);
                 console.log("Airports by country", search.result);
                 setCountrySearch(search.result);
+
+                // Add the chart integration code here
+                const sdk = new ChartsEmbedSDK({
+                    baseUrl: "https://charts.mongodb.com/charts-project-0-itpum",
+                });
+
+                const chart = sdk.createChart({
+                    chartId: "656308f1-7ac8-44b7-8d63-eaefcb4d5267",
+                    height: "800px",
+                    filter: {country: selectedCountryCode}
+                    
+                    
+                });
+                const chart1 = sdk.createChart({
+                    chartId: "65630fd4-9ab1-4689-8249-82c8468b68f1",
+                    height: "800px",
+                    filter: {country: selectedCountryCode}
+                });
+                await chart.render(document.getElementById("chart"));
+                await chart1.render(document.getElementById("chart1"))
+               
+               
+                
+
+                
+
+
+                
                  
               
                 
@@ -46,9 +81,45 @@ function App() {
     return user && db && user.state === "active" ? (
         <>
         <CitySearch setCountryCode={setSelectedCountryCode} countrySearch={countrySearch}/>
-        <AirportList countrySearch={countrySearch} airports={airports} user={user} logOut={logOut} />
+        <br></br>
+        <div style={{display: 'flex'}}>
+    <iframe
+        style={{
+            background: '#FFFFFF',
+            border: 'none',
+            borderRadius: '10px',
+            boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
+            marginLeft: '80px',
+            marginRight: '40px',  // Adjust the margin between frames as needed
+        }}
+        width="640"
+        height="480"
+        src="https://charts.mongodb.com/charts-project-0-itpum/embed/charts?id=656317ff-f4f9-4bb5-8848-c74168af8ad3&maxDataAge=3600&theme=light&autoRefresh=true"
+    />
+
+
+
+    <iframe
+        style={{
+            background: '#21313C',
+            border: 'none',
+            borderRadius: '10px',
+            boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
+        }}
+        width="640"
+        height="480"
+        src="https://charts.mongodb.com/charts-project-0-itpum/embed/charts?id=65631eec-e05a-4583-834a-769a588371d7&maxDataAge=3600&theme=dark&autoRefresh=true"
+    />
+</div>
+
+       
+   
+        <AirportList countrySearch={countrySearch} user={user} logOut={logOut} />
+        <br></br>
         
-        
+        <div id="chart"></div>
+        <div id="chart1"></div>
+     
         </>
         
 
