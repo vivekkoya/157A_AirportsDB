10ed187934be9385770c36e2adb3a310e58c54fb
vivekkoya <13vivekkoya@gmail.com>
2023-11-25 21:10:41 -0800

    run successful build

--- client/src/App.js
+++ client/src/App.js
@@ -148,9 +148,9 @@ function AirportGraph(props){
 const renderLineChart = (
   <LineChart width={600} height={300} data={data}>
     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    <CartesianGrid stroke="#ccc" />
-    <XAxis dataKey="name" />
-    <YAxis />
+    {/* <CartesianGrid stroke="#ccc" /> */}
+    {/* <XAxis dataKey="name" /> */}
+    {/* <YAxis /> */}
   </LineChart>
 );
     return (
